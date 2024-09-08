5f9327474544f5669d95aaee377f822a57236d11
Gabriel Hernández <desarrolloweb@cuanda.com.mx>
2024-09-07 18:49:32 -0600

    Actualizar dependencia de Prettier a la versión 3.3.3

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,14 +8,4 @@ export async function GET(context) {
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>es-mx</language>`,
   });
-}import rss, { pagesGlobToRssItems } from '@astrojs/rss';
-
-export async function GET(context) {
-  return rss({
-    title: 'Alumno de Astro | Blog',
-    description: 'Mi viaje de aprendizaje de Astro',
-    site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
-  });
 }
