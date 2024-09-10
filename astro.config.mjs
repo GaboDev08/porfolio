a005d29744b7d1eee93336ee32e0da783fb9a724
Gabriel Hernández <desarrolloweb@cuanda.com.mx>
2024-09-10 01:07:15 -0600

    Conflictos resueltos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-<<<<<<< HEAD
-// https://astro.build/config
-export default defineConfig({});
-=======
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
@@ -11,4 +7,3 @@ export default defineConfig({
   site: "https://example.com",
   integrations: [preact()]
 });
->>>>>>> master
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-/// <reference path="../.astro/types.d.ts" />
-=======
+
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
->>>>>>> master
